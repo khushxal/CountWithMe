d7eb8184d7c73077abe38bf7a0339e762ac88eae
Khushxal <vkkhushal18@gmail.com>
2024-08-14 13:05:11 +0530

    Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-
+import { isMobile } from "react-device-detect";
 function Header() {
   return (
     <header className="container">
       <nav className="navbar">
         <div className="container d-flex justify-content-center">
-          <a className="navbar-brand text-uppercase" href="/">
+          <a
+            className={`navbar-brand text-uppercase ${
+              isMobile && "text-white"
+            }}`}
+            href="/"
+          >
             CountWithMee
           </a>
         </div>
