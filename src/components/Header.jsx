033ff2bdac99be83803f7a4c2e76e3a9ca5840f4
Khushxal <vkkhushal18@gmail.com>
2024-08-14 13:10:21 +0530

    Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { isMobile } from "react-device-detect";
+import { BrowserView, MobileView } from "react-device-detect";
 function Header() {
   return (
     <header className="container">
       <nav className="navbar">
         <div className="container d-flex justify-content-center">
-          <a
-            className={`navbar-brand text-uppercase ${
-              isMobile && "text-white"
-            }}`}
-            href="/"
-          >
-            CountWithMee
-          </a>
+          <BrowserView>
+            <a className={`navbar-brand text-uppercase`} href="/">
+              CountWithMee
+            </a>
+          </BrowserView>
+          <MobileView>
+            <a className={`navbar-brand text-uppercase text-white`} href="/">
+              CountWithMee
+            </a>
+          </MobileView>
         </div>
       </nav>
     </header>
